33061c532da2788e8186d75e6cd588ba85a4cdf1
esmasui <esmasui@beb32e34-ab3b-11de-9787-55e617d4dd53>
2009-10-13 11:58:15 +0000

    prepare for post message.

---
+++ trunk/DroidSensor/src/org/sevenleaves/droidsensor/PostMessageActivity.java
@@ -0,0 +1,15 @@
+package org.sevenleaves.droidsensor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PostMessageActivity extends Activity {
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.post_message);
+	}
+}
