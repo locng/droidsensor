8c3e5db052d8105b25e458e7994a7f0c167ce6b4
esmasui <esmasui@beb32e34-ab3b-11de-9787-55e617d4dd53>
2009-10-15 11:43:32 +0000

    add copyright

--- trunk/BluetoothExample/src/com/example/bluetooth/scanmode/DiscoverableContinuasActivity.java
+++ trunk/BluetoothExample/src/com/example/bluetooth/scanmode/DiscoverableContinuasActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2009, DroidSensor - http://code.google.com/p/droidsensor/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.bluetooth.scanmode;
 
 import android.app.Activity;
@@ -9,7 +25,7 @@ import com.example.bluetooth.BluetoothDeviceStubFactory;
 public class DiscoverableContinuasActivity extends Activity {
 
 	private DiscoverableContinuasReceiver _receiver;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 
@@ -27,10 +43,10 @@ public class DiscoverableContinuasActivity extends Activity {
 		_receiver = new DiscoverableContinuasReceiver(bluetooth);
 		_receiver.registerSelf(this);
 	}
-	
+
 	@Override
 	protected void onDestroy() {
-	
+
 		super.onDestroy();
 		unregisterReceiver(_receiver);
 	}
