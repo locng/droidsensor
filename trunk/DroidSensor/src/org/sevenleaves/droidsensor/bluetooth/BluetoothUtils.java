740f93293588169d0c3ebcdeff8f0f62857f311f
esmasui <esmasui@beb32e34-ab3b-11de-9787-55e617d4dd53>
2009-10-31 20:23:43 +0000

    resolve device name with minor device class.

--- trunk/DroidSensor/src/org/sevenleaves/droidsensor/bluetooth/BluetoothUtils.java
+++ trunk/DroidSensor/src/org/sevenleaves/droidsensor/bluetooth/BluetoothUtils.java
@@ -95,42 +95,225 @@ public abstract class BluetoothUtils {
 
 	public static String getMajorDeviceClassName(Context context, int deviceClass) {
 
-		int i = BluetoothClass.Device.Major.getDeviceMajor(deviceClass);
+		int i = BluetoothClass.Device.getDevice(deviceClass);
 		int res;
 
 		switch (i) {
-		case BluetoothClass.Device.Major.MISC:
-			res = R.string.bt_major_misc;
-			break;
-		case BluetoothClass.Device.Major.COMPUTER:
-			res = R.string.bt_major_computer;
-			break;
-		case BluetoothClass.Device.Major.PHONE:
-			res = R.string.bt_major_phone;
-			break;
-		case BluetoothClass.Device.Major.NETWORKING:
-			res = R.string.bt_major_networking;
-			break;
-		case BluetoothClass.Device.Major.AUDIO_VIDEO:
-			res = R.string.bt_major_audio_video;
-			break;
-		case BluetoothClass.Device.Major.PERIPHERAL:
-			res = R.string.bt_major_peripheral;
-			break;
-		case BluetoothClass.Device.Major.IMAGING:
-			res = R.string.bt_major_imaging;
-			break;
-		case BluetoothClass.Device.Major.WEARABLE:
-			res = R.string.bt_major_wearable;
-			break;
-		case BluetoothClass.Device.Major.TOY:
-			res = R.string.bt_major_toy;
-			break;
-		case BluetoothClass.Device.Major.HEALTH:
-			res = R.string.bt_major_health;
-			break;
+        // Devices in the COMPUTER major class
+        case BluetoothClass.Device.COMPUTER_UNCATEGORIZED:
+        	
+        	res = R.string.class_COMPUTER_UNCATEGORIZED;
+        	break;
+        case BluetoothClass.Device.COMPUTER_DESKTOP:
+        	
+        	res = R.string.class_COMPUTER_DESKTOP;
+        	break;
+        case BluetoothClass.Device.COMPUTER_SERVER:
+        	
+        	res = R.string.class_COMPUTER_SERVER;
+        	break;
+        case BluetoothClass.Device.COMPUTER_LAPTOP:
+        	
+        	res = R.string.class_COMPUTER_LAPTOP;
+        	break;
+        case BluetoothClass.Device.COMPUTER_HANDHELD_PC_PDA:
+        	
+        	res = R.string.class_COMPUTER_HANDHELD_PC_PDA;
+        	break;
+        case BluetoothClass.Device.COMPUTER_PALM_SIZE_PC_PDA:
+        	
+        	res = R.string.class_COMPUTER_PALM_SIZE_PC_PDA;
+        	break;
+        case BluetoothClass.Device.COMPUTER_WEARABLE:
+        	
+        	res = R.string.class_COMPUTER_WEARABLE;
+        	break;
+
+        // Devices in the PHONE major class
+        case BluetoothClass.Device.PHONE_UNCATEGORIZED:
+        	
+        	res = R.string.class_PHONE_UNCATEGORIZED;
+        	break;
+        case BluetoothClass.Device.PHONE_CELLULAR:
+        	
+        	res = R.string.class_PHONE_CELLULAR;
+        	break;
+        case BluetoothClass.Device.PHONE_CORDLESS:
+        	
+        	res = R.string.class_PHONE_CORDLESS;
+        	break;
+        case BluetoothClass.Device.PHONE_SMART:
+        	
+        	res = R.string.class_PHONE_SMART;
+        	break;
+        case BluetoothClass.Device.PHONE_MODEM_OR_GATEWAY:
+        	
+        	res = R.string.class_PHONE_MODEM_OR_GATEWAY;
+        	break;
+        case BluetoothClass.Device.PHONE_ISDN:
+        	
+        	res = R.string.class_PHONE_ISDN;
+        	break;
+        // Minor classes for the AUDIO_VIDEO major class
+        case BluetoothClass.Device.AUDIO_VIDEO_UNCATEGORIZED:
+        	
+        	res = R.string.class_AUDIO_VIDEO_UNCATEGORIZED;
+        	break;
+        case BluetoothClass.Device.AUDIO_VIDEO_WEARABLE_HEADSET:
+        	
+        	res = R.string.class_AUDIO_VIDEO_WEARABLE_HEADSET;
+        	break;
+        case BluetoothClass.Device.AUDIO_VIDEO_HANDSFREE:
+        	
+        	res = R.string.class_AUDIO_VIDEO_HANDSFREE;
+        	break;
+        //case BluetoothClass.Device.AUDIO_VIDEO_RESERVED              = 0x040C;
+        case BluetoothClass.Device.AUDIO_VIDEO_MICROPHONE:
+        	
+        	res = R.string.class_AUDIO_VIDEO_MICROPHONE;
+        	break;
+        case BluetoothClass.Device.AUDIO_VIDEO_LOUDSPEAKER:
+        	
+        	res = R.string.class_AUDIO_VIDEO_LOUDSPEAKER;
+        	break;
+        case BluetoothClass.Device.AUDIO_VIDEO_HEADPHONES:
+        	
+        	res = R.string.class_AUDIO_VIDEO_HEADPHONES;
+        	break;
+        case BluetoothClass.Device.AUDIO_VIDEO_PORTABLE_AUDIO:
+        	
+        	res = R.string.class_AUDIO_VIDEO_PORTABLE_AUDIO;
+        	break;
+        case BluetoothClass.Device.AUDIO_VIDEO_CAR_AUDIO:
+        	
+        	res = R.string.class_AUDIO_VIDEO_CAR_AUDIO;
+        	break;
+        case BluetoothClass.Device.AUDIO_VIDEO_SET_TOP_BOX:
+        	
+        	res = R.string.class_AUDIO_VIDEO_SET_TOP_BOX;
+        	break;
+        case BluetoothClass.Device.AUDIO_VIDEO_HIFI_AUDIO:
+        	
+        	res = R.string.class_AUDIO_VIDEO_HIFI_AUDIO;
+        	break;
+        case BluetoothClass.Device.AUDIO_VIDEO_VCR:
+        	
+        	res = R.string.class_AUDIO_VIDEO_VCR;
+        	break;
+        case BluetoothClass.Device.AUDIO_VIDEO_VIDEO_CAMERA:
+        	
+        	res = R.string.class_AUDIO_VIDEO_VIDEO_CAMERA;
+        	break;
+        case BluetoothClass.Device.AUDIO_VIDEO_CAMCORDER:
+        	
+        	res = R.string.class_AUDIO_VIDEO_CAMCORDER;
+        	break;
+        case BluetoothClass.Device.AUDIO_VIDEO_VIDEO_MONITOR:
+        	
+        	res = R.string.class_AUDIO_VIDEO_VIDEO_MONITOR;
+        	break;
+        case BluetoothClass.Device.AUDIO_VIDEO_VIDEO_DISPLAY_AND_LOUDSPEAKER:
+        	
+        	res = R.string.class_AUDIO_VIDEO_VIDEO_DISPLAY_AND_LOUDSPEAKER;
+        	break;
+        case BluetoothClass.Device.AUDIO_VIDEO_VIDEO_CONFERENCING:
+        	
+        	res = R.string.class_AUDIO_VIDEO_VIDEO_CONFERENCING;
+        	break;
+        //case BluetoothClass.Device.AUDIO_VIDEO_RESERVED              = 0x0444;
+        case BluetoothClass.Device.AUDIO_VIDEO_VIDEO_GAMING_TOY:
+        	
+        	res = R.string.class_AUDIO_VIDEO_VIDEO_GAMING_TOY;
+        	break;
+
+        // Devices in the WEARABLE major class
+        case BluetoothClass.Device.WEARABLE_UNCATEGORIZED:
+        	
+        	res = R.string.class_WEARABLE_UNCATEGORIZED;
+        	break;
+        case BluetoothClass.Device.WEARABLE_WRIST_WATCH:
+        	
+        	res = R.string.class_WEARABLE_WRIST_WATCH;
+        	break;
+        case BluetoothClass.Device.WEARABLE_PAGER:
+        	
+        	res = R.string.class_WEARABLE_PAGER;
+        	break;
+        case BluetoothClass.Device.WEARABLE_JACKET:
+        	
+        	res = R.string.class_WEARABLE_JACKET;
+        	break;
+        case BluetoothClass.Device.WEARABLE_HELMET:
+        	
+        	res = R.string.class_WEARABLE_HELMET;
+        	break;
+        case BluetoothClass.Device.WEARABLE_GLASSES:
+        	
+        	res = R.string.class_WEARABLE_GLASSES;
+        	break;
+
+        // Devices in the TOY major class
+        case BluetoothClass.Device.TOY_UNCATEGORIZED:
+        	
+        	res = R.string.class_TOY_UNCATEGORIZED;
+        	break;
+        case BluetoothClass.Device.TOY_ROBOT:
+        	
+        	res = R.string.class_TOY_ROBOT;
+        	break;
+        case BluetoothClass.Device.TOY_VEHICLE:
+        	
+        	res = R.string.class_TOY_VEHICLE;
+        	break;
+        case BluetoothClass.Device.TOY_DOLL_ACTION_FIGURE:
+        	
+        	res = R.string.class_TOY_DOLL_ACTION_FIGURE;
+        	break;
+        case BluetoothClass.Device.TOY_CONTROLLER:
+        	
+        	res = R.string.class_TOY_CONTROLLER;
+        	break;
+        case BluetoothClass.Device.TOY_GAME:
+        	
+        	res = R.string.class_TOY_GAME;
+        	break;
+
+        // Devices in the HEALTH major class
+        case BluetoothClass.Device.HEALTH_UNCATEGORIZED:
+        	
+        	res = R.string.class_HEALTH_UNCATEGORIZED;
+        	break;
+        case BluetoothClass.Device.HEALTH_BLOOD_PRESSURE:
+        	
+        	res = R.string.class_HEALTH_BLOOD_PRESSURE;
+        	break;
+        case BluetoothClass.Device.HEALTH_THERMOMETER:
+        	
+        	res = R.string.class_HEALTH_THERMOMETER;
+        	break;
+        case BluetoothClass.Device.HEALTH_WEIGHING:
+        	
+        	res = R.string.class_HEALTH_WEIGHING;
+        	break;
+        case BluetoothClass.Device.HEALTH_GLUCOSE:
+        	
+        	res = R.string.class_HEALTH_GLUCOSE;
+        	break;
+        case BluetoothClass.Device.HEALTH_PULSE_OXIMETER:
+        	
+        	res = R.string.class_HEALTH_PULSE_OXIMETER;
+        	break;
+        case BluetoothClass.Device.HEALTH_PULSE_RATE:
+        	
+        	res = R.string.class_HEALTH_PULSE_RATE;
+        	break;
+        case BluetoothClass.Device.HEALTH_DATA_DISPLAY:
+        	
+        	res = R.string.class_HEALTH_DATA_DISPLAY;
+        	break;
+
 		default:
-			res = R.string.bt_major_uncategorized;
+			res = R.string.class_UNCATEGORIZED;
 			break;
 		}
 
